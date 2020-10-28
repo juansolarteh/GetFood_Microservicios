883a8dfa18233fb76a778c460bad7e96a827fd47
jeffersoncy <48133501+jeffersoncy@users.noreply.github.com>
2020-10-27 22:41:46 -0500

    GET-15
    Se crearon y  agregan las interfaces graficas de: listar_plato, agregar_plato, utilizando la herramienta de framework Angular

---
+++ domiciliosRest/src/app/Persona/add/add.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+//import { ServiceService } from 'src/app/Service/service.service';
+
+@Component({
+  selector: 'app-add',
+  templateUrl: './add.component.html',
+  styleUrls: ['./add.component.css']
+})
+export class AddComponent implements OnInit {
+
+  constructor(){}
+  //constructor(private router:Router, private service:ServiceService) { }
+
+  ngOnInit(): void {
+  }
+  Guardar(){}
+}
