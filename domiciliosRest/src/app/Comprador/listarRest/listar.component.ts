35a0e31fab0c1fcd8fe227dee6a596bff485d4d4
juansolarteh <juansolarteh@unicauca.edu.co>
2020-11-06 13:28:49 -0500

    botones
    deshabilitar botones de restaurantes cerrados y switch para cambio de estado del restaurante

--- domiciliosRest/src/app/Comprador/listarRest/listar.component.ts
+++ domiciliosRest/src/app/Comprador/listarRest/listar.component.ts
@@ -3,12 +3,15 @@ import { Router } from '@angular/router';
 import { Restaurante } from 'src/app/Modelo/Restaurante';
 import { ListarComponent } from 'src/app/Administrador/listar/listar.component';
 import { ServiceService } from '../../Service/service.service';
+import { $ } from 'protractor';
+import { AbstractControl } from '@angular/forms';
 
 @Component({
   selector: 'app-listarRest',
   templateUrl: './listar.component.html',
   styleUrls: ['./listar.component.css']
 })
+
 export class ListarRestComponent implements OnInit{
   restaurantes:Restaurante[];
   constructor(private service:ServiceService, private router:Router) { }
